b84631708645ea3d63fc9809ab1b49aebbcecddf
NoCluckingWay <m.anyjobseanpresent@gmail.com>
2026-09-05 17:18:15 -0500

    Update cache version from v4 to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='coop-tracker-v4';
+const CACHE='coop-tracker-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./supabase.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
